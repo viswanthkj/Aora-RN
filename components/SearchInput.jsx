ec91ae2964a5846143f8ffdef75c4ec74c1f0668
viswanthkj <viswanth1995@gmail.com>
2024-11-22 12:15:32 +0530

    feat-search

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet, Image } from "react-native";
+import { View, Text, TextInput, StyleSheet, Image, Alert } from "react-native";
 import { icons } from "../constants";
 import { TouchableOpacity } from "react-native";
+import { usePathname, router } from "expo-router";
 
 const SearchInput = ({
-  value,
-  onChangeText,
-  placeholder,
-  keyboardType,
+  initialQuery
 }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
   return (
     <View className="w-full border-2 border-black-100 h-16 rounded-2xl bg-black-100 px-5 mt-4 focus:border-secondary-200 flex-row items-center">
-      <TextInput
-        className="flex-1 text-white font-psemibold focus:border-secondary"
-        value={value}
-        onChangeText={onChangeText}
-        placeholder={placeholder}
-        placeholderTextColor={"#7B7B8B"}
-        keyboardType="default"
-        autoCapitalize="none"
+       <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={query}
+        placeholder="Search a video topic"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
       />
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity 
+         onPress={() => {
+          if (query === "")
+             return Alert.alert(
+              "Missing Query",
+              "Please input something to search results across database"
+            );
+
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}>
           <Image
             source={icons.search}
             className="h-[25px] w-[25px]"
